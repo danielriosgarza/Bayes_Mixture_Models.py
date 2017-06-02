058a72f475f2ba24534d21a68959b99cbdd9225b
Daniel Garza <danielriosgarza@gmail.com>
2017-06-02 17:24:11 +0200

    Update Gaussian_component.py

--- Gaussian_component.py
+++ Gaussian_component.py
@@ -3,56 +3,36 @@ import numpy as np
 import math
 import scipy.linalg as linalg
 from Cholesky import Cholesky
+from Gaussian_variable import Gaussian_variable
 import random
 trm = linalg.get_blas_funcs('trmm')
 
 
-class Gaussian_component:
+
+class Gaussian_component(Gaussian_variable):
     
     '''Deals with multiple measurements of a Gaussian variable random variable.'''
     
-    def __init__(self, d, kappa_0 = 0, v_0=0, mu_0=None, S_0=None, X=None, method='cov'):
+    def __init__(self, d, kappa_0 = 0, v_0=0, mu_0=None, S_0=None, X=None):
         
-        if X is None:
-            self.n =0
-        elif d==1:
-            self.n = len(X.flatten())
-        else:
-            self.n=len(X)
         
-        self.d = d
         
-        self.method=method
+        
+        self.d = self._Gaussian_variable__d(d)
+
+        self.n = self.__n(X)
         
         self.kappa_0 = kappa_0
         
         self.v_0 = v_0
         
-        self.GI = Gaussian_variable(self.d, mu = mu_0, S = S_0, method=self.method)
+        self.mu_0 = self._Gaussian_variable__mu(mu_0)
         
-        if mu_0 is None:
-            self.mu_0 = self.GI.mu
-        else:
-            self.mu_0 = mu_0
         
-        if S_0 is None:
-            self.S_0 = self.GI.S
-        else:
-            self.S_0 = S_0 
-        if d==1:
-            self.mu_0=float(np.array(self.mu_0).flatten())
-            self.S_0=float(np.array(self.S_0).flatten())
-        if X is None:
-            self.X = self.GI.Xi
-            self.X.shape=(1,self.d)
+        self.S_0 = self._Gaussian_variable__S(S_0)
         
-        elif d==1:
-            self.X = X
-            X.shape = (len(X.flatten()),1)
-            
-            
-        else:
-            self.X = X 
+        
+        self.X = self.__X(X)
         
         self.sX = None
         
@@ -61,6 +41,10 @@ class Gaussian_component:
         self.emp_mu = self.__emp_mu()
         
         self.scale = None
+
+        self.inv_scale = None
+        
+        self.chol_inv_scale = None
         
         self.XX_T=None
         
@@ -72,13 +56,45 @@ class Gaussian_component:
         
         self.chol_prec=None
 
+    def __n(self, X=None):
+        if X is None:
+            self.n =0
+        elif self.d==1:
+            self.n = len(X.flatten())
+        else:
+            self.n=len(X)
+        return self.n
     
+    def __X(self, X=None):
+        if X is None:
+            self.X = self._Gaussian_variable__Xi()
+            if self.d>1:
+                self.X.shape=(1,self.d)
         
+        elif self.d==1:
+            if X.shape!=(self.d,0):
+                print '\x1b[5;31;46m'+'Warning: data was flattened'+ '\x1b[0m'
+            self.X = X
+            self.X.shape = (len(X.flatten()),1)
+            
+            
+        else:
+            self.X = X
+            self.X.shape = (self.n, self.d)
+        
+        return self.X
+    
     def __sX(self, X=None):
         if self.n == 0:
-            self.sX = np.zeros(self.d)
+            if self.d==1:
+                self.sX=0
+            else:
+                self.sX = np.zeros(self.d)
         elif self.n == 1:
-            self.sX = self.X.flatten()
+            if self.d==1:
+                self.sX = float(self.X)
+            else:
+                self.sX = self.X.flatten()
         else:
             if self.d==1:
                 self.sX = float(np.einsum('ij->j', self.X))
@@ -87,12 +103,13 @@ class Gaussian_component:
         return self.sX
     
     def __mu(self):
-        if self.n is 0:
+        if self.n == 0:
             self.mu = self.mu_0 
-            return self.mu
+            
         else:
             self.mu = (self.kappa_0*self.mu_0 + self.__sX(self.X))/(self.kappa_0+self.n)
-            return self.mu
+        
+        return self.mu
     
     def __emp_mu(self):
         if self.n is 0:
@@ -112,30 +129,48 @@ class Gaussian_component:
         return self.XX_T
         
     def __scale(self):
-        if self.n is 0:
-            if self.d==1:
-                self.scale = float(self.GI._Gaussian_variable__cov())
-                
-            else:
-                self.scale=self.GI._Gaussian_variable__cov() + self.kappa_0*np.einsum('i,j->ji', self.mu_0, self.mu_0)
+        if self.n == 0:
+            self.scale = self.S_0
                 
         else:
             
             if self.d==1:
-                self.scale = float(np.array(self.GI._Gaussian_variable__cov()).flatten()+self.__XX_T() + self.kappa_0*(self.mu_0**2)-(self.kappa_0+self.n)*(self.mu**2))
+                self.scale = self.S_0 + self.__XX_T() + self.kappa_0*(self.mu_0**2)-(self.kappa_0+self.n)*(self.mu**2)
             
             else:
-                self.scale= self.GI._Gaussian_variable__cov()+self.__XX_T()+self.kappa_0*np.einsum('i,j->ji', self.mu_0, self.mu_0)-(self.kappa_0+self.n)*np.einsum('i,j->ji', self.mu, self.mu)
+                self.scale= self.S_0 + self.__XX_T() + self.kappa_0*np.einsum('i,j->ji', self.mu_0, self.mu_0)-(self.kappa_0+self.n)*np.einsum('i,j->ji', self.mu, self.mu)
         
         return self.scale
     
+    def __inv_scale(self):
+        if self.scale is None:
+            self.scale = self.__scale()
+        if self.d==1:
+            self.inv_scale=1./self.scale
+        else:
+            self.inv_scale = Cholesky(self.scale)._Cholesky__inv()
+        
+        return self.inv_scale
+    
+    def __chol_inv_scale(self):
+        if self.inv_scale is None:
+            self.inv_scale = self.__inv_scale()
+        if self.d==1:
+            self.chol_inv_scale=self.inv_scale
+        else:
+            self.chol_inv_scale = Cholesky(self.inv_scale).lower
+        
+        return self.chol_inv_scale
+    
     def __cov(self):
-        if self.n is 0:
+        '''Empirical covariance matrix'''
+
+        if self.n == 0:
             if self.d==1:
-                self.cov = float(self.GI._Gaussian_variable__cov())
+                self.cov = 1.
                 
             else:
-                self.cov=self.GI._Gaussian_variable__cov() + self.kappa_0*np.einsum('i,j->ji', self.mu_0, self.mu_0)
+                self.cov= 1. + self.kappa_0*np.einsum('i,j->ji', self.mu_0, self.mu_0)
                 
         else:
             
@@ -148,30 +183,98 @@ class Gaussian_component:
         return self.cov
             
     def __chol_cov(self):
-        if self.d==1:
-            self.chol_cov = self.__cov()
-            return self.chol_cov
-        else:
-            self.chol_cov= Cholesky(self.__cov()).lower()
-            return self.chol_cov
+        cov = self.__cov()
+        self.chol_cov = Gaussian_variable(d=self.d, S=cov)._Gaussian_variable__chol_cov()
+        return self.chol_cov
         
     def __prec(self):
-        if self.d==1:
-            self.prec = 1./self.__cov()
-            return self.prec
-        else:
-            self.prec= Cholesky(self.__cov()).inv()
-            return self.prec
+        cov = self.__cov()
+        self.prec = Gaussian_variable(d=self.d, S=cov)._Gaussian_variable__prec()
+        return self.prec
     
     def __chol_prec(self):
-        if self.d==1:
-            self.chol_prec = self.__prec()
-            return self.chol_prec
+        cov = self.__cov()
+        self.chol_prec = Gaussian_variable(d=self.d, S=cov)._Gaussian_variable__chol_prec()
+        return self.chol_prec
+
+    def chol_prec_rvs(self):
+        if self.v_0+self.n<self.d+1:
+            df = self.d+1
         else:
-            self.chol_prec = Cholesky(self.__cov()).chol_of_the_inv()
-            return self.chol_prec
+            df = self.v_0+self.n
+        
+        if self.d==1:
+            
+            if self.scale is None:
+                s = self.__scale()
+            else:
+                s=self.scale
+            
+            return random.gammavariate(0.5*df, 2./s)
+        
+        else:
+                    
+            if self.chol_inv_scale is None:
+                a = self.__chol_inv_scale()
+            else:
+                a= self.chol_inv_scale
+            
+            ind = np.tril_indices(self.d,-1) #lower triangular non-diagonal elements
+            
+            B = np.zeros((self.d,self.d)) 
+            
+            norm = np.random.standard_normal(len(ind[0])) #normal samples for the lower triangular non-diagonal elements
+    
+            B[ind] = norm 
+    
+            chisq = [math.sqrt(random.gammavariate(0.5*(df-(i+1)+1), 2.0)) for i in xrange(self.d)]
+    
+            B = B+np.diag(chisq)
+    
+            ch_d =trm(alpha=1, a=a, b=B, lower=1)
+    
+            dg= np.diag(ch_d)#Assuring the result is the Cholesky decomposition that contains positive diagonals
+    
+            adj = np.tile(dg/abs(dg), (self.d,1))
+    
+            return ch_d*adj
+
+    def prec_rvs(self):
+        if self.d==1:
+            return 1./self.chol_prec_rvs()
+        else:
+            return Cholesky(self.chol_prec_rvs(), method='lower').matrix
+
+    def cov_rvs(self):
+        
+        if self.d==1:
+            return 1./self.chol_prec_rvs()
+        else:
+            return Cholesky(self.chol_prec_rvs(), method='lower')._Cholesky__inv()
+    
+    def chol_cov_rvs(self):
+        
+        if self.d==1:
+            return 1./self.chol_prec_rvs()
+        else:
+            return Cholesky(self.chol_prec_rvs(), method='lower')._Cholesky__chol_of_the_inv()
+
+    def mu_rvs(self, chol_prec=None):
+        
+        if chol_prec is None:
+            chol_prec = self.chol_prec_rvs()
+        
+        if self.n==0:
+            return Gaussian_variable(d= self.d, mu=self.mu, S = chol_prec, method='chol_prec').rvs().flatten()
+        else:
+            return Gaussian_variable(d= self.d, mu=self.mu, S = math.sqrt(self.kappa_0+self.n)*chol_prec, method='chol_prec').rvs().flatten()
 
     
+    def rvs(self, n=1):
+        chol_prec = self.chol_prec_rvs()
+        mu = self.mu_rvs(chol_prec)
+        return Gaussian_variable(self.d, mu=mu, S = chol_prec, method='chol_prec').rvs().flatten()
+        
     def s_down_date(self, ind_X, cov=False, chol_cov=False, prec=False, chol_prec=False):
         
         if self.n is 0:
@@ -360,31 +463,10 @@ class Gaussian_component:
         
         
         
-    def chol_prec_rvs(self):
-        if self.v_0+self.n<self.d+1:
-            df = self.d+1
-        else:
-            df = self.v_0+self.n
-        
-        if self.chol_prec is None:
-            a = self.__chol_prec()
-        ind = np.tril_indices(self.d,-1) #lower triangular non-diagonal elements
-        B = np.zeros((self.d,self.d)) 
-        norm = np.random.standard_normal(len(ind[0])) #normal samples for the lower triangular non-diagonal elements
-        B[ind] = norm 
-        chisq = [math.sqrt(random.gammavariate(0.5*(df-(i+1)+1), 2.0)) for i in xrange(self.d)]
-        B = B+np.diag(chisq)
-        ch_d =trm(alpha=1, a=self.chol_prec, b=B, lower=1)
-        dg= np.diag(ch_d)#Assuring the result is the Cholesky decomposition that contains positive diagonals
-        adj = np.tile(dg/abs(dg), (self.d,1))
-        return ch_d*adj
+    
 
-    def mu_rvs(self):
-        
-        return Gaussian_variable(self.d, mu=self.mu, S = math.sqrt(self.kappa_0+self.n)*self.chol_prec_rvs(), method='chol_prec').rvs()
-
-    def rvs(self, n):
-        return Gaussian_variable(self.d, mu=self.mu_rvs(), S = self.chol_prec_rvs(), method='chol_prec').rvs(n)
+    
+    
 
     def rvs_Gibbs(self, n):
         s = {i:Gaussian_variable(self.d, mu=self.mu_rvs(), S = self.chol_prec_rvs(), method='chol_prec').rvs()[0] for i in xrange(n)}
